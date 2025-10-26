8fdad3ceb46029331222036c8131f4b734125600
Eleazar Curiel <eleazar.isc@gmail.com>
2025-10-26 14:30:53 -0600

    postgres añadido

--- index.mjs
+++ index.mjs
@@ -1,6 +1,14 @@
 import express from "express";
 import cors from "cors"
+import  pg from "pg";
+import {config} from "dotenv";
 
+config();
+
+const pool = new pg.Pool({
+  connectionString:process.env.DATABASE_URL,
+  ssl:true
+})
 // Configuración del servidor
 // -------------------
 const app = express();
@@ -10,6 +18,13 @@ app.use(express.json());
 app.get('/', (req, res)=>{
   res.send("Hello word")
 })
+
+app.get('/sales', async (req, res)=>{
+  const response = await pool.query('SELECT NOW()')
+  console.log(response);
+  return res.json(response.rows[0])
+})
+
 // -------------------
 // Iniciar servidor
 // -------------------
